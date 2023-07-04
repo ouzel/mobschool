3fe642a49455e069d79a81d70d4b9e7b1e6938bd
LAPTOP-LMHGTNO8\Sophie <sadrozd@edu.hse.ru>
2023-07-04 16:29:09 +0300

    first comm

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -72,5 +72,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+        // dsfyailuf
     }
 }
